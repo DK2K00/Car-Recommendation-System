b4b28f5acd99449c93a4faca42e9c53ca9f81e51
Danush Kumar <danushkumar27@gmail.com>
2021-04-14 17:19:02 +0530

    For testing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -16,11 +17,21 @@ app.get('/', (req,res) => {
     res.render('index');
 })
 
+app.get('/recommend', (req,res) => {
+    res.render('recommend');
+})
+
+
+app.post('/recommend', (req,res) => {
+    res.render('recommend');
+})
 
 
 
 
 
-app.listen(8080, () => {
-    console.log('Serving on port 8080')
+
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
 })
